1a22a88ab668cf72369a37858898e4732994b8c9
zhangxiaokang <zhangxiaokang@baimaohui.net>
2026-01-11 15:45:49 +0800

    change

--- whisper/whisperDemo/record.py
+++ whisper/whisperDemo/record.py
@@ -1,32 +1,73 @@
 import pyaudio
 import queue
 
+# 选择监听的音频设备
 input_device_index = -1
+
 # 配置录音参数
 CHUNK = 1024
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
 RATE = 16000
 
+# 设定的说话间隔时间
+Internal = 16000
+
+# 指定音频转文字模型
+VoiceToWordModel = 'small'
+
+# 源语言
+SourceLanguage = "ja"
+
+# 目的语言
+TargetLanguage = "zh"
+
+# 载入语音活动检测模型
 import torch
 torch.hub._validate_not_a_forked_repo = lambda a, b, c: True
 vad_model, funcs = torch.hub.load(
             repo_or_dir="snakers4/silero-vad", model="silero_vad", trust_repo=True
         )
 detect_speech = funcs[0]
+# print(torch.cuda.is_available())   # True 表示 GPU 可用
+# if torch.cuda.is_available():
+#     device = torch.device("cpu")
+#     print("使用 GPU:", torch.cuda.get_device_name(0))
+# else:
+#     device = torch.device("cpu")
+#     print("使用 CPU")
+
+# 调用翻译API
+import requests
+def mymemory_translate(text, source=SourceLanguage, target=TargetLanguage):
+    url = "https://api.mymemory.translated.net/get"
+    params = {
+        "q": text,
+        "langpair": f"{source}|{target}"
+    }
+    r = requests.get(url, params=params)
+    return r.json()["responseData"]["translatedText"]
+
 
 import whisper
 import opencc
 import numpy as np
+import time
 cc = opencc.OpenCC('t2s')
-model = whisper.load_model("base")
-options = whisper.DecodingOptions(language='zh')
+model = whisper.load_model(VoiceToWordModel)
+# model = model.to(device)
+options = whisper.DecodingOptions(language=None, task="transcribe", fp16=torch.cuda.is_available())
 
+# 翻译音频并输出中文
 def get_audio_text(audio_data):
+    start_time = time.time()
     audio = whisper.pad_or_trim(audio_data)
     mel = whisper.log_mel_spectrogram(audio).to(model.device)
     result = whisper.decode(model, mel, options)
-    print(cc.convert(result.text))
+    end_time = time.time()
+    print("原文:" + result.text)
+    print("翻译结果:" + cc.convert(mymemory_translate(result.text)))
+    print(f"翻译耗时:{end_time - start_time:.3f}s")
 
 
 # 创建队列用于存储录音数据
@@ -40,9 +81,9 @@ def detect_voice_activity(audio):
     ) # 检测语音活动
     print(speeches,len(audio)) # 打印检测到的语音片段和音频长度
 
-    if len(speeches) == 2: # 如果检测到两段语音，检查它们是否相近
-        if speeches[1]['start'] - speeches[0]['end'] < 8000:
-            return [{"start": speeches[0]['start'], "end": speeches[1]['end']}]
+    # if len(speeches) == 2: # 如果检测到两段语音，检查它们是否相近
+    #     if speeches[1]['start'] - speeches[0]['end'] < 8000:
+    #         return [{"start": speeches[0]['start'], "end": speeches[1]['end']}]
     return speeches
 
 def recording_callback(in_data, frame_count, time_info, status):
@@ -55,13 +96,13 @@ def record():
     # 检测系统扬声器
     # 检测系统中所有可以监听的音频设备
     for i in range(p.get_device_count()):
-        dev = p.get_device_info_by_index(i)
-        print(dev)
-        # if '立体声混音' in dev['name']:
-        #     input_device_index = i
-        #     break
+        info = p.get_device_info_by_index(i)
+        # print(info)
+        if "CABLE" in info["name"] and info["maxInputChannels"] > 0:
+            print("USE THIS:", i, info["name"])
+
     # 指定监听的音频源，监听的是系统音频输出
-    input_device_index = 2
+    input_device_index = 3
     # 获取音频流
     stream = p.open(format=FORMAT,
                     channels=CHANNELS,
@@ -79,6 +120,7 @@ def record():
     temp = np.array([],np.float32) # 存储临时的录音数据
     data = b'' # 存储从队列中获取的字节数据
     Recording = False # 录音状态标志
+    LastEnd = 0 # 上一段录音的结束位
 
     while True:
         # 从队列中获取录音数据
@@ -92,8 +134,8 @@ def record():
         if len(speeches) == 0: # 没有检测到语音
             if Recording:
                 Recording = False
-                alldata = np.concatenate((alldata,temp), axis=0) # 合并录音数据
-                get_audio_text(alldata) # 处理录音数据
+                if len(alldata) > 0:
+                    get_audio_text(alldata) # 处理录音数据
 
             # 重置变量
             alldata = np.array([],np.float32)
@@ -105,44 +147,73 @@ def record():
             start = int(speeches[0]['start']) 
             end = int(speeches[0]['end'])
 
-            if start < 8000:
+            if start + LastEnd < Internal:
                 # 这是一句话
-                alldata = np.concatenate((alldata,temp[:end]), axis=0)
-                temp = temp[end:]
+                alldata = np.concatenate((alldata,temp[start:end]), axis=0)
+                LastEnd = len(temp[end:])
                 # get_audio_text(alldata)
                 # 重置变量
                 data = b''
+                temp = np.array([],np.float32)
             else:
                 # 这是两句话
                 # 先处理前一句话
                 if len(alldata) > 0:
                     get_audio_text(alldata)
                 
-                alldata = temp[:end]
-                temp = temp[end:]
+                alldata = temp[start:end]
+                LastEnd = len(temp[end:])
+
+                # 重置变量
                 data = b''
+                temp = np.array([],np.float32)
 
         elif len(speeches) == 2:
             Recording = True
-            start2 = int(speeches[0]['start']) 
-            end2 = int(speeches[0]['end'])
+            start = int(speeches[0]['start']) 
+            end = int(speeches[0]['end'])
+            start2 = int(speeches[1]['start'])
+            end2 = int(speeches[1]['end'])
 
             # 获取并添加第一句话之后再处理
-            if start2 < 8000:
+            if start + LastEnd < Internal:
                 # 这是一句话
-                alldata = np.concatenate((alldata,temp[:end2]), axis=0)
-                get_audio_text(alldata)
+                alldata = np.concatenate((alldata,temp[start:end]), axis=0)
+                if start2 - end < Internal:
+                    alldata = np.concatenate((alldata, temp[start2:end2]), axis=0)
+                    LastEnd = len(temp[end2:])
+                    
+                    # 重置变量
+                    data = b''
+                    temp = np.array([],np.float32)
+                else:
+                    get_audio_text(alldata)
+                    alldata = temp[start2:end2]
+                    LastEnd = len(temp[end2:])
+
+                    # 重置变量
+                    data = b''
+                    temp = np.array([],np.float32)
+
             else:
                 # 这是两句话
                 if len(alldata) > 0:
                     get_audio_text(alldata)
-                alldata = temp[start2:end2]
-                get_audio_text(alldata)
+                alldata = temp[start:end]
+                if start2 - end < Internal:
+                    alldata = np.concatenate((alldata, temp[start2:end2]), axis=0)
+                    LastEnd = len(temp[end2:])
+                    
+                    # 重置变量
+                    data = b''
+                    temp = np.array([],np.float32)
+                else:
+                    get_audio_text(alldata)
+                    alldata = temp[start2:end2]
+                    LastEnd = len(temp[end2:])
 
-
-            # 重置变量，保留第二句话的数据
-            temp = temp[end2:]
-            alldata = np.array([],np.float32)
-            data = b''
+                    # 重置变量
+                    data = b''
+                    temp = np.array([],np.float32)
 
 record()
